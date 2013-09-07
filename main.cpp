728d1cc1146455aa125254421b75ed3a7a069696
srb0203 <srb0203@gmail.com>
2013-09-06 23:21:26 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,27 +1,40 @@
 #include <iostream>
 #include "Relation.h"
 #include "Database.h"
+#include "Attribute.h"
+
 using namespace std;
 
 int main(){
   Database d;
+  d.Create("Cars");
+  d.AddColumn("Cars",Header("Make",STRING));
+  d.AddColumn("Cars",Header("Year",INT));
+  d.AddColumn("Cars",Header("Model",STRING));
+  vector<Cell> cell;
+  cell.push_back("Audi");
+  cell.push_back("2013");
+  cell.push_back("R8");
+  d.Insert("Cars", cell);
+  cell.clear();
+  cell.push_back("Audi2");
+  cell.push_back("2013222222");
+  cell.push_back("R8222222");
+  d.Insert("Cars", cell);
 
-  Relation r;
-  r.Insert("Audi", 2013,"R8");
-  r.Insert("Chevrolet", 2011,"Cobalt");
-  r.Insert("Mercedes",2010,"Benz");
-  r.Insert("Dodge",2009,"Charger");
-  r.Insert("Nissan", 2006 , "Altima");
+  d.Create("LOL");
+  d.AddColumn("LOL",Header("Hello",STRING));
+  d.AddColumn("LOL",Header("Bello",STRING));
+  
+  vector<Cell> cell1;
+  cell1.push_back("lol");
+  cell1.push_back("lol2");
+  d.Insert("LOL",cell1);
 
-  //r.Insert("add",5);
-  //r.Insert("sub",6);
-
-  cout << "\nNumber of attributes " << r.getNumAttributes() << endl;
-
-  r.dis_table();
-
-
-
-  //d.Show();
+  d.Show("Cars");
+  d.Rename("Cars","sfsd","Model");
+  d.Show("Cars");
+  d.Show("LOL");
+  //d.Show("kjlk");
   return 0;
 }
