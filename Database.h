470b49c179edcdf0a60a59c17d8b3b2c5b4d2420
srb0203 <srb0203@gmail.com>
2013-09-06 23:20:07 -0500

    Update Database.h

--- Database.h
+++ Database.h
@@ -1,15 +1,21 @@
 #include <iostream>
+#include <map>
 #include "Relation.h"
 
 using namespace std;
 
 class Database {
   public:
-  Database(){};
+    typedef map<string,Relation> Table;
     vector<Relation> relation;
-
-    void Create();
-    void Insert();
+  Database(){};
+    Table table;
+    void Selection(Relation name, string attr_name);
+    void Create(string rel_name);
+    void AddColumn(const string& rel_name, const Header& h);
+    void Insert(string rel_name, const vector<Cell>& row);
     void Write();
-    void Show();
+    void Show(string rel_name);
+    void Rename(string rel_name,string new_name, string old_name);
+    Relation& operator[](const string& s);
 };
