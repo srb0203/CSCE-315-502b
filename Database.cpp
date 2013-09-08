ad4d94f2f4c4aabb591b8454a419a9c38b47bca8
srb0203 <srb0203@gmail.com>
2013-09-07 20:27:23 -0500

    Update Database.cpp
    Updating Database.cpp after adding delete and union function.

--- Database.cpp
+++ Database.cpp
@@ -1,7 +1,19 @@
 #include "Database.h"
+#include <algorithm> 
 void Database:: Selection(Relation name, string attr_name) {
 
 }
+
+void Database::Delete_attr(const string& rel_name,const string& attribute) {
+ for (int i = 0; i < relation.size(); ++i){
+    if (rel_name == relation[i].name){
+      relation[i].Delete_attr(attribute);
+      return;
+    }
+  }
+  cerr << "I did not find anything called "<< rel_name << endl;
+}
+ 
 
 void Database:: Rename(string rel_name,string new_name, string old_name) {
   for (int i = 0; i < relation.size(); ++i){
@@ -37,7 +49,8 @@ void Database:: Create(string rel_name) {
   relation.push_back(Relation(rel_name));
 }
 
-void Database:: Write() {
+void Database:: Write(const string& rel_name) {
+  cout << "Requires file I/O, will be done in the next part " << endl;
 }
 
 void Database:: Show(string rel_name) {
@@ -51,4 +64,36 @@ void Database:: Show(string rel_name) {
 }
 Relation& Database::operator[](const string& s){
   return table[s]; 
+}
+
+void Database::Union(const string& rel_name1,const string& rel_name2) {
+  int i1,i2;
+  for (int i = 0; i < relation.size(); ++i){
+    if (rel_name1 == relation[i].name){
+      i1 =i;
+      break;
+    }
+  }
+  for (int i = 0; i < relation.size(); ++i){
+    if (rel_name2 == relation[i].name){
+      i2 = i;
+      break;
+    }
+  }
+  Create(rel_name1+rel_name2+"Union");
+  bool insrt = true;
+  
+  int i1Rowsize = relation[i1].getRowSize();
+  int jrowsize = relation[i2].getRowSize();
+  for(int j=0; j < jrowsize; j++) {
+    for(int i=0; i < relation[i1].getRowSize(); i++) {
+      if(relation[i2].getRow(j) == relation[i1].getRow(i)) {
+        break;
+      }
+      if(i==jrowsize-1){
+        Insert(relation[i1].name,relation[i2].getRow(j));
+        break;
+      }
+    }
+  } 
 }
