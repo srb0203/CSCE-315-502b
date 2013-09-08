498bc5266ecfbc5173232eb7b8eed53de7cb78c2
srb0203 <srb0203@gmail.com>
2013-09-07 20:28:03 -0500

    Update Database.h

--- Database.h
+++ Database.h
@@ -14,8 +14,10 @@ class Database {
     void Create(string rel_name);
     void AddColumn(const string& rel_name, const Header& h);
     void Insert(string rel_name, const vector<Cell>& row);
-    void Write();
+    void Write(const string& rel_name);
     void Show(string rel_name);
     void Rename(string rel_name,string new_name, string old_name);
     Relation& operator[](const string& s);
+    void Delete_attr(const string& rel_name,const string& attribute);
+    void Union(const string& rel_name1,const string& rel_name2);
 };
