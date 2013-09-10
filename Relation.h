7e33414c1cc85fb0b363a33dfebfc77227b38fcb
srb0203 <srb0203@gmail.com>
2013-09-09 20:57:49 -0500

    Update Relation.h

--- Relation.h
+++ Relation.h
@@ -13,42 +13,42 @@ using namespace std;
 //Tuples: -Attribute- -Attribute- -Attribute- ...etc
 //--------------------------------------------------------------------
 
-class Relation{
+class Relation{//relation has a vector of attributes and a name. This is basically a "Table"
   public:
     vector< Attribute > attr;
     string name;
     Relation() {}
-    Relation(const string& name){
+    Relation(const string& name){ //constructor which initializes name
       Create(name);
     }
-    void add_attr(const Attribute& a) {
+    void add_attr(const Attribute& a) {//pushes back an attribute
       attr.push_back(a);
     }
-    void add_attr(string nm, Type t) {
+    void add_attr(string nm, Type t) {//adds an attribute with defined types and names
       add_attr(Attribute(t,nm));
     }
-    Attribute& operator[](size_t col){
+    Attribute& operator[](size_t col){//returns an attribute
       return attr[col];
     }
-    Attribute& getAttribute(int n)
+    Attribute& getAttribute(int n)//another simpler return function (different people used different things for now)
     {
       return (*this)[n];
     }
-	int findAttribute(string attr_name){
+	int findAttribute(string attr_name){//searches for a specific attribute and returns its int location else it returns -1 for not found
 		for(int i = 0; i < attr.size(); i++){
 			if(attr_name == attr[i].getName())
 				return i;
 		}
 		return -1;
 	}
-    int getNumAttributes()
+    int getNumAttributes()//attr size value
     {
       return attr.size();
     }
-	Type getAttributeType(int n) {
+	Type getAttributeType(int n) {//returns the attributes defined type
       return attr[n].type;
     } 
-    vector<Cell> getRow(int n)
+    vector<Cell> getRow(int n)// returns a tuple
     {
       vector<Cell> tuple;
       for(int i = 0; i < attr.size(); i++)
@@ -58,17 +58,17 @@ class Relation{
       return tuple;
     }
 
-    int getRowSize() {
+    int getRowSize() {//returns the column length dispite the name (havent changed)
       return attr[0].getLength();
     }
 
-    void read(string s);
+    void read(string s);//not needed yet
 
-    void Create(string rel_name){
+    void Create(string rel_name){//sets a name
       name = rel_name;
     }
 
-    void Show() {
+    void Show() {//the function that displays data in relation. Messy but looks nice in the command window
 	if(attr.size() !=0){
 		  vector<Cell> tuple;
 		  cout << endl;
@@ -104,7 +104,7 @@ class Relation{
 		cerr << "Relation '" << name << "' has no members." << endl;
     }
 
-    void Rename(string new_name, string old_name) {
+    void Rename(string new_name, string old_name) {//renames an attribute with a new name
       for(int i =0; i < attr.size(); i++) {
         if(old_name == attr[i].name) {
           attr[i].name = new_name;
@@ -112,7 +112,7 @@ class Relation{
       }
     }
 
-    void Insert(vector<Cell> row) { 
+    void Insert(vector<Cell> row) { //inserts a tuple
       for(int c = 0; c< attr.size(); c++)
         attr[c].push_back(row[c]);
     }
@@ -120,12 +120,12 @@ class Relation{
     void Write() {
       cout << "Requires File I/O, will be implemented in the next part " << endl;
     }
-	void clear_attr_cells(){
+	void clear_attr_cells(){//clears the whole attribute
 		for (int i = 0; i < attr.size(); ++i){
 			attr[i].cell.clear();
 		}
 	}
-    void Delete_attr(const string& attribute) {
+    void Delete_attr(const string& attribute) {//deletes a whole attribute
       for(int i =0; i < attr.size(); i++) {
         for(int j = 0; j < attr[0].getLength(); j++) {
           if(attribute == attr[i][j]) {
