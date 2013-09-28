d90be382f51d1693a7791c67cfe8748b0b750f11
srb0203 <srb0203@gmail.com>
2013-09-27 21:44:19 -0500

    Update DBApp.cpp

--- DBApp.cpp
+++ DBApp.cpp
@@ -1,10 +1,40 @@
 #include "DBApp.h"
 
 void DBApp:: CreateTable() {
-	p.parse("CREATE TABLE cars (carid INTEGER, name VARCHAR(1), model VARCHAR(1), year INTEGER, engine_type INTEGER) PRIMARY KEY (carid);");
+	p.parse("CREATE TABLE cars (Car_ID INTEGER, Name VARCHAR(1), model VARCHAR(1), year INTEGER, engine_type INTEGER) PRIMARY KEY (carid);");
 	p.parse("INSERT INTO cars VALUES FROM (5,\"Joe\", \"cat\", 4,6);");
 	p.parse("CREATE TABLE hello (carid INTEGER, name VARCHAR(1), model VARCHAR(1), year INTEGER, engine_type INTEGER) PRIMARY KEY (carid);");
 	p.parse("INSERT INTO cars VALUES FROM (7,\"bob\", \"the builder\", 46,67);");
+	p.parse("CREATE TABLE Shipments (Car_ID INTEGER, Name VARCHAR(1), model VARCHAR(1), year INTEGER, engine_type INTEGER) PRIMARY KEY (carid);");
+	p.parse("INSERT INTO cars VALUES FROM (001,\"Audi\", \"R8\", 2013,34);");
+	p.parse("INSERT INTO cars VALUES FROM (002,\"Chevrolet\", \"Cobalt\", 2013,36);");
+	p.parse("INSERT INTO cars VALUES FROM (003,\"Nissan\", \"Altima\", 2011,38);");
+	p.parse("INSERT INTO cars VALUES FROM (004,\"Audi\", \"A8\", 2014,43);");
+	p.parse("INSERT INTO Shipments VALUES FROM (004,\"Audi\", \"A8\", 2015,43);");
+	p.parse("INSERT INTO Shipments VALUES FROM (004,\"Audi\", \"A8\", 2015,43);");
+}
+
+
+void DBApp:: AddCar(string table_name) {
+	string carID, name, model, year, engine;
+	cout << "Enter the Name of the car " << endl;
+	cin >> name;
+	cout << "Enter the Car ID of the car " << endl;
+	cin >> carID; 
+	cout << "Enter the Model the car " << endl;
+	cin >> model;
+	cout << "Enter the Year of the car " << endl;
+	cin >> year;
+	cout << "Enter the engine type of the car " << endl;
+	cin >> engine;
+	
+	p.parse("INSERT INTO "+table_name+" VALUES FROM ("+carID+", "+ "\"" + name +"\"" + ", " + "\"" + model +"\"" +", " + year +", "+engine+");");
+	cout << "Values have been added :) " << endl;
+}
+
+void DBApp:: Diff() {
+	p.parse("NotInWarehouse <- Shipments - cars;");
+	p.parse("SHOW NotInWarehouse;");
 }
 
 void DBApp:: UpdateCar(string table_name) {
@@ -40,14 +70,20 @@ void DBApp:: DeleteCar(string car_name, string table_name) {
 	return;
 }
 
-void DBApp:: ShowCustomers(string table_name) {
-	p.parse("Customers <- project (carid,name) cars;");
+void DBApp:: ShowCarWithID(string table_name) {
+	p.parse("Cars_ID <- project (carid,name) cars;");
+	p.parse("SHOW Cars_ID;");
 	return;
 }
 
+void DBApp:: Exit() {
+	p.parse("EXIT;");
+}
+
 void DBApp:: test() {
-	//p.parse("UPDATE cars SET name = 0 WHERE name == Joe;");
-	p.parse("DELETE FROM cars WHERE (name == Joe);");
+	p.parse("UPDATE cars SET Name = 0 WHERE Name == Chevrolet;");
+	//p.parse("DELETE FROM cars WHERE (Name == Audi);");
+	p.parse("SHOW cars;");
 }
 	
 int main() {
@@ -56,59 +92,76 @@ int x;
 
  DBApp d;
  d.CreateTable();
- d.test();
+ //d.test();
 // d.CreateTable();
- // while(true) {
-		// cout << "\nThis is a simple Database Application " << endl;
-		// cout << "\nPlease choose what you would like to do" << endl;
+ while(true) {
+		cout << "\nThis is a simple Database Application " << endl;
+		cout << "\nPlease choose what you would like to do" << endl;
 
-		// cout << "1. Add Cars the to the warehouse " << endl;
-		// cout << "2. Delete a Car from the warehouse " << endl;
-		// cout << "3. Show from the warehouse " << endl;
-		// cout << "4. Update information about a Car in the warehouse " << endl;
-		// cout << "5. Show table of cars " << endl;
-		// cout << "6. Exit " << endl;
-		// cin >> x;
+		cout << "1. Add Cars the to the warehouse or Shipments " << endl;
+		cout << "2. Delete a Car from the warehouse " << endl;
+		cout << "3. Show Cars and their ID " << endl;
+		cout << "4. Update information about a Car in the warehouse " << endl;
+		cout << "5. Show table of cars " << endl;
+		cout << "6. Show the cars that are not in the warehouse but are in the shipment " << endl;
+		cout << "7. Exit " << endl;
+	
+		cin >> x;
 		
-		// if(x == 2) {
-			// string car_name,table_name;
-			// cout <<"Enter the name of the table from which the car needs to be deleted " << endl;
-			// cin >> table_name;
-			// cout << "Enter the name of the car to be deleted " << endl;
-			// cin >> car_name;
-			// d.DeleteCar(car_name,table_name);
-			// cout << "\n....Returning to the main menu " << endl;
-		// }
+		if(x == 1) {
+			string table_name;
+			cout << "Enter the name of the table you want to add the cars to, cars or Shipments" << endl;
+			cin >> table_name;
+			d.AddCar(table_name);
+		}
 		
-		// if(x == 3) {
-			// string table_name;
-			// cout << "Enter the name of the table you want to select from " << endl;
-			// cin >> table_name;
-			// d.ShowCustomers(table_name);
-		// }
+		if(x == 2) {
+			string car_name,table_name;
+			cout <<"Enter the name of the table from which the car needs to be deleted " << endl;
+			cin >> table_name;
+			cout << "Enter the name of the car to be deleted " << endl;
+			cin >> car_name;
+			d.DeleteCar(car_name,table_name);
+			cout << "\n....Returning to the main menu " << endl;
+		}
 		
-		// if(x == 4) {
-			// string table_name;
-			// cout << "Enter the name of the table you wnat to update " << endl;
-			// cin >> table_name;
-			// d.UpdateCar(table_name);
-		// }
+		if(x == 3) {
+			string table_name;
+			cout << "Enter the name of the table you want to select from " << endl;
+			cin >> table_name;
+			d.ShowCarWithID(table_name);
+		}
 		
-		// if(x == 5) {
-			// string name;
-			// cout << "Enter the name of the table to show " << endl;
-			// cin >> name;
-			// d.ShowTable(name);
-			// cout << "\n....Returning to the main menu " << endl;
-		// }			
+		if(x == 4) {
+			string table_name;
+			cout << "Enter the name of the table you want to update " << endl;
+			cin >> table_name;
+			d.UpdateCar(table_name);
+		}
 		
-		// if(x == 6) {
-			// cout << " \n         Good Bye :) " << endl;
-			// exit(0);
-			//parser.parse("EXIT");
-		// }	
-		// else {
-			// cout << "Invalid Input, Please choose from the list. " << endl;
-		// }
-	// }
+		if(x == 5) {
+			string name;
+			cout << "Enter the name of the table to show " << endl;
+			cin >> name;
+			d.ShowTable(name);
+			cout << "\n....Returning to the main menu " << endl;
+		}			
+		
+		if(x == 6) {
+			d.Diff();
+		}
+		
+		if(x == 7) {
+			cout << " \n         Good Bye :) " << endl;
+			d.Exit();
+		}	
+		
+		if(x == 8) {
+			d.test();
+		}
+		
+		else {
+			cout << "Invalid Input, Please choose from the list. " << endl;
+		}
+	}
 }
