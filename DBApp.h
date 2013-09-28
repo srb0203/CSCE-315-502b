d70ce1da98a1be1f97dd5597817715f65f5f8e69
srb0203 <srb0203@gmail.com>
2013-09-27 21:45:09 -0500

    Update DBApp.h

--- DBApp.h
+++ DBApp.h
@@ -1,16 +1,26 @@
+#ifndef DBAPP_H
+#define DBAPP_H
+
 #include <iostream>
 #include <stdlib.h>
+#include "Parser.h"
 
 using namespace std;
 
 class DBApp {
-	//parser p;
+	Parser p;
 	
 	public:
 		void CreateTable();
 		void AddNewCar();
-		void DeleteCar(string car_name,string table_name);
-		void UpdateCar();
-		void SelectCar();
+		void DeleteCar(string car_name, string table_name);
+		void UpdateCar(string table_name);
+		void ShowCarWithID(string table_name);
 		void ShowTable(string tablename);
+		void test();
+		void AddCar(string table_name);
+		void Exit();
+		void Diff();
 };
+
+#endif
