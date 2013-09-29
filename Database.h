5ceee9b484ba672e9906583ff0164c7dd7813e53
srb0203 <srb0203@gmail.com>
2013-09-29 18:54:52 -0500

    Update Database.h

--- Database.h
+++ Database.h
@@ -8,37 +8,32 @@
 
 using namespace std;
 
-class Database {
-  public:
-	typedef map<string,Relation> Table;
-	vector<Relation> relation;
-	Relation Selection;
-	Relation Projection;
-	Database(){Create("Expression");}
-	//Database(){};
-	Table table;
-	void SelectAttribute(string attr_name1, string condition, string attr_name2, string rel_name);
-	void SelectLiteral(string attr_name, string condition, string cell_condition, string rel_name);
-	void Update(string rel_name, string attr_name, string literal, string rel_name2);
-	void Delete(string rel_name, string condition_attr, string condition, string condition_literal);
-	void Project(vector<string> attr_name, string rel_name);
-	void Create(string rel_name);
-	//void Delete(string rel_name);
-	void AddColumn(const string& rel_name, const Header& h);
-	void Insert(string rel_name, const vector<Cell>& row);
-	void Open(const string& rel_name);
-	void Write(const string& rel_name);
-	void Show(string rel_name);
-	Relation Copy_table(const string rel_name1, const string rel_name2);
-	//void Rename(strding rel_name,string new_name, string old_name);
-	void Rename(const string& newrel_name, const string& rel_name,vector<string> attr_names);
-	Relation& operator[](const string& s);
-	void Delete_attr(const string& rel_name,const string& attribute);
-	void Union(const string& rel_name1,const string& rel_name2, const string& rel_name3);
-	void Intersection(const string& rel_name1,const string& rel_name2, const string& rel_name3);
-	void Difference(const string& rel_name1, const string& rel_name2, const string& rel_name3);
-	void Product(const string& rel_name1, const string& rel_name2, const string& rel_name3);
+class Database
+{
+private:
+	vector<Relation*> relations;
+public:
+	int size() { return relations.size(); }
+	bool has_relation(string name);
+	void push_back(Relation* r) { relations.push_back(r); }
+	void set_relation(string name, Relation* r);
+	void write(string name);
+	void close();
+	void open(string name);
+	Relation* get_relation(int i) { return relations[i]; }
+	Relation* get_relation(string name);
+	Relation* create(string name, vector<Attribute> attributes, vector<string> primary);
+	Relation* insert(string name, vector<string> literal_list);
+	Relation* insert(string name, Relation* expr);
+	Relation* select_literal(string attribute_name, string op, string literal, Relation* expr);
+	Relation* select_attribute(string attribute_name1, string op, string attribute_name2, Relation* expr);
+	Relation* project(vector<string> attribute_list, Relation* atomic_expr);
+	Relation* update(vector<string> attribute_list, vector<Cell> cells, Relation* atomic_expr);
+	Relation* rename(vector<string> attribute_list, Relation* atomic_expr);
+	Relation* _union(Relation* r1, Relation* r2);
+	Relation* intersection(Relation* r1, Relation* r2);
+	Relation* difference(Relation* r1, Relation* r2);
+	Relation* product(Relation* r1, Relation* r2);
 };
-
 
 #endif
