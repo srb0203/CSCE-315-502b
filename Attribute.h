d12a4a558453c5a44aca23ac4eb31f48121ba1e4
srb0203 <srb0203@gmail.com>
2013-09-29 18:52:55 -0500

    Update Attribute.h

--- Attribute.h
+++ Attribute.h
@@ -5,46 +5,41 @@
 
 using namespace std;
 
-typedef string Cell;
+enum Type { INT = 1, STRING };
 
-enum Type {INT, STRING};
+struct Cell
+{
+	string value;
+	Type type;
+};
 
-typedef pair<string, Type> Header;
-
-class Attribute{
-  public:
-    string name;
-  public:
-    Type type;
-    vector<string> cell;
-    Attribute () {};
-    Attribute(Type t, string n) : type(t), name(n) {};
-   
-    void push_back(string v)
-    {
-      cell.push_back(v);
-    }
-    vector<string> getCells() const{
-      return cell;
-    }
-    string getName()
-    {
-      return name;
-    }
-	Type getType(){
-		return type;
+class Attribute
+{
+private:
+	string name;
+	Type type;
+	vector<Cell> cells;
+public:
+	Attribute() {}
+	Attribute(Type t, string n) : type(t), name(n) {}
+	
+	int size() { return cells.size(); }
+	string get_name() { return name; }
+	Type get_type() { return type; }
+	Cell get(int i) { return cells[i]; }
+	Cell operator[](size_t i) { return get(i); }
+	vector<Cell> get_cells() { return cells; }
+	void clear() { cells.clear(); }
+	void push_back(Cell c) { cells.push_back(c); }
+	void push_back(string v, Type t)
+	{
+		Cell c;
+		c.value = v;
+		c.type = t;
+		cells.push_back(c);
 	}
-	int getLength() const{
-		return cell.size();
-	}
-	void setName(string set_name)
-    {
-      name = set_name;
-    }
-
-  Cell& operator[](size_t row){
-    return cell[row];
-  }
+	void insert(int i, Cell c) { cells.insert(cells.begin() + i, c); }
+	void set_name(string n) { name = n; }
 };
 
 #endif
